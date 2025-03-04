0f3bbf790b74ecf8a73702577043815c1d15b2b0
ahmadfirdosh008 <ahmadfirdosh008@gmail.com>
2025-03-04 14:39:52 +0530

    updated the car_detection

--- System/Functions/Detection.py
+++ System/Functions/Detection.py
@@ -1,22 +1,11 @@
 import cv2
 from PIL import Image
-from Car_Detection.detect import detect_image
-from Car_Detection.darknet import Darknet
-import numpy as np
-#from Car_Detection_TF.yolo import YOLO
-from Mosse_Tracker.TrackerManager import Tracker
-
 
 class Detection:
-    def __init__(self,yolo):
+    def __init__(self, yolo):
         self.yolo = yolo
 
-#uncomment these two lines if you will use yolo
-        # self.model = Darknet("M:/Argus/Car_Detection/config/yolov3.cfg", CUDA=False)
-        # self.model.load_weight("M:/Argus/Car_Detection/config/yolov3.weights")
-
-
-    def detect(self,frames,frame_width,frame_height,read_file,boxes_file = None, read_file_self=False, tf=True):
+    def detect(self, frames, frame_width, frame_height, read_file, boxes_file=None, read_file_self=False, tf=True):
         boxes = []
         # detect vehicles
         if read_file_self:
@@ -26,6 +15,7 @@ class Detection:
             img = Image.fromarray(frames[0])
             _, boxes = self.yolo.detect_image(img)
         else:
-            boxes = detect_image(frames[0], self.model)
+            # This branch is no longer used since we're removing PyTorch
+            raise NotImplementedError("PyTorch detection has been removed")
 
         return boxes
