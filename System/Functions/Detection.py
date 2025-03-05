9581880d1c9c0b56f594e29724e9bd17f8b5d8ae
ahmadfirdosh008 <ahmadfirdosh008@gmail.com>
2025-03-05 09:54:31 +0530

    dectction cleaned

--- System/Functions/Detection.py
+++ System/Functions/Detection.py
@@ -2,20 +2,47 @@ import cv2
 from PIL import Image
 
 class Detection:
+    """
+    Class responsible for vehicle detection using YOLO or file-based detection
+    """
+    
     def __init__(self, yolo):
+        """
+        Initialize detector
+        
+        Args:
+            yolo: YOLO model instance for detection
+        """
         self.yolo = yolo
 
     def detect(self, frames, frame_width, frame_height, read_file, boxes_file=None, read_file_self=False, tf=True):
+        """
+        Detect vehicles in frames
+        
+        Args:
+            frames: List of video frames
+            frame_width: Width of frames
+            frame_height: Height of frames
+            read_file: Boolean indicating whether to read from file
+            boxes_file: Pre-determined boxes from file
+            read_file_self: Override to force file-based detection
+            tf: Use TensorFlow model for detection
+            
+        Returns:
+            boxes: List of detected vehicle bounding boxes
+        """
         boxes = []
-        # detect vehicles
+        
+        # Choose detection method based on parameters
         if read_file_self:
-            # From files
+            # Use pre-determined boxes from file
             boxes = boxes_file
         elif tf:
+            # Use TensorFlow YOLO model for detection
             img = Image.fromarray(frames[0])
             _, boxes = self.yolo.detect_image(img)
         else:
-            # This branch is no longer used since we're removing PyTorch
+            # This branch has been removed as PyTorch detection is not supported
             raise NotImplementedError("PyTorch detection has been removed")
 
         return boxes
